4aac7859d0a9242fe20f5c23c60e1aeceba449a2
IcaroCaetano <icaro.caetano@gmail.com>
2025-07-29 22:12:28 -0300

    first commit

---
+++ src/main/java/com/myproject/poc_guarded_string/PocGuardedStringApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.poc_guarded_string;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocGuardedStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocGuardedStringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/poc_guarded_string/PocGuardedStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.poc_guarded_string;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocGuardedStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
